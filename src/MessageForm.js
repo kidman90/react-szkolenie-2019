7a4ccd3d119a2cf3153094ac83a550754b144980
kidman90 <kacprzak90@gmail.com>
2019-03-12 13:05:57 +0100

    11

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
-export const MessageForm = () =>
-  <input type="text" onChange={inputHandler} />;
+export const MessageForm = () => {
+  const textInput = React.createRef();
 
-const inputHandler = e => console.log(e.target.value);
+  const inputHandler = e => console.log(e.target.value);
+
+  const submitHandler = e => {
+    e.preventDefault();
+    alert(textInput.current.value);
+    textInput.current.value = '';
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input type="text" ref={textInput} onChange={inputHandler} />
+    </form>
+  );
+};
