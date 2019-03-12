7490a6b66adf1f2c78108da7c6300641605614af
kidman90 <kacprzak90@gmail.com>
2019-03-12 12:39:54 +0100

    09

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Message } from './Message';
 
-export const App = () =>
-  <Message
-    // userName="Maciek"
-    time={Date.now()}
-    message="To jest przykładowa wiadomość :)"
-  />;
+export const App = (props) => {
+  const { data } = props;
+
+  return data.map(elem => <Message key={elem.userName} {...elem} />);
+};
+
+App.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({ ...Message.propTypes }))
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+const data = [
+  {
+    userName: 'BTM',
+    time: Date.now(),
+    message: 'Witaj na szkoleniach z React!'
+  },
+  {
+    userName: 'Gość',
+    time: Date.now(),
+    message: 'Hej!'
+  }
+];
+
 ReactDOM.render(
-  <App />
+  <App data={data} />
   , document.getElementById('root')
 );
