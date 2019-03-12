273becc4f2ffb7255fb04562adb5f45692fa9526
kidman90 <kacprzak90@gmail.com>
2019-03-12 09:28:36 +0100

    04

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const App = () => (
+  <p>
+    Witaj na <b>Warsztatach React</b>!
+  </p>
+);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-export const App = () => (
-  <p>
-    Witaj na <b>Warsztatach React</b>!
-  </p>
-);
+import { App } from './App';
 
 ReactDOM.render(
   <App />
