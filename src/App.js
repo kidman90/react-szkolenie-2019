ef661ba1a468523e1db1d1b312a9067f4df6b516
kidman90 <kacprzak90@gmail.com>
2019-03-13 12:09:53 +0100

    16

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Message } from './Message';
 import { MessageForm } from './MessageForm';
 
-export const App = ({ data }) => (
-  <React.Fragment>
-    {data.map((elem, index) => <Message key={index} {...elem} />)}
-    <MessageForm onMessage={message => alert(message)} />
-  </React.Fragment>
-);
+export class App extends Component {
+  state = {
+    data: this.props.data
+  };
+
+  onMessage = message => this.setState(prevState => ({
+    data: prevState.data.concat({
+      userName: 'BTM',
+      message,
+      time: Date.now()
+    })
+  }));
+
+  render() {
+    if (!this.state.data || !this.state.data.constructor === Array) {
+      return 'Trwa pobieranie danych';
+    }
+
+    return (
+      <React.Fragment>
+        {!this.state.data.length
+          ? (
+            <p>Brak danych</p>
+          ) : (
+            this.state.data.map((elem, index) => <Message key={index} {...elem} />)
+          )
+        }
+        <MessageForm onMessage={this.onMessage} />
+      </React.Fragment>
+    );
+  }
+};
 
 App.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({ ...Message.propTypes }))
--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
-const data = [
-  {
-    userName: 'BTM',
-    time: Date.now(),
-    message: 'Witaj na szkoleniach z React!'
-  },
-  {
-    userName: 'Gość',
-    time: Date.now(),
-    message: 'Hej!'
-  }
-];
+// const data = [
+//   {
+//     userName: 'BTM',
+//     time: Date.now(),
+//     message: 'Witaj na szkoleniach z React!'
+//   },
+//   {
+//     userName: 'Gość',
+//     time: Date.now(),
+//     message: 'Hej!'
+//   }
+// ];
+const data = [];
 
 ReactDOM.render(
   <App data={data} />
+  // <App />
   , document.getElementById('root')
 );
